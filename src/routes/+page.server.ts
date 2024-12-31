5761833a9e3447aa1637b62376119e1b915e44c5
Dwight Van Tuyl <dvantuyl@gmail.com>
2024-12-30 21:29:42 -0800

    merge image data with festival data

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,6 +4,27 @@ import fs from 'fs';
 import path from 'path';
 import toml from 'toml';
 
+const mapImages = import.meta.glob(
+  '/static/images/maps/*.{avif,gif,heif,jpeg,jpg,png,tiff,webp,svg}',
+  {
+    eager: true,
+    query: {
+      enhanced: true
+    }
+  }
+)
+
+const artistImages = import.meta.glob(
+  '/static/images/artists/*.{avif,gif,heif,jpeg,jpg,png,tiff,webp,svg}',
+  {
+    eager: true,
+    query: {
+      enhanced: true
+    }
+  }
+)
+
+
 export const load: PageServerLoad = async () => {
   const daysPath = path.resolve('src/data/days.toml');
   const artistsPath = path.resolve('src/data/artists.toml');
@@ -14,6 +35,37 @@ export const load: PageServerLoad = async () => {
   const artists = toml.parse(fs.readFileSync(artistsPath, 'utf-8'));
   const stages = toml.parse(fs.readFileSync(stagesPath, 'utf-8'));
   const options = toml.parse(fs.readFileSync(optionsPath, 'utf-8'));
+
+  // Add map images to day objects
+  Object.entries(mapImages).forEach(([path, file]) => {
+    const imageFilenameWithoutExtension = path.split('/').pop()?.split('.').shift();
+    const typedFile = file as { default: { img: { src: string, w: number, h: number } } };
+
+    if (!imageFilenameWithoutExtension || !days[imageFilenameWithoutExtension]) {
+      return;
+    }
+
+    days[imageFilenameWithoutExtension].mapImage = {
+      src: typedFile.default.img.src,
+      width: typedFile.default.img.w,
+      height: typedFile.default.img.h,
+    };
+  });
+
+  // Add artist images to artist objects
+  Object.entries(artistImages).forEach(([path, file]) => {
+    const imageFilenameWithoutExtension = path.split('/').pop()?.split('.').shift();
+    const typedFile = file as { default: { img: { src: string, w: number, h: number } } };
+
+    if (!imageFilenameWithoutExtension || !artists[imageFilenameWithoutExtension]) {
+      return;
+    }
+    artists[imageFilenameWithoutExtension].image = {
+      src: typedFile.default.img.src,
+      width: typedFile.default.img.w,
+      height: typedFile.default.img.h
+    };
+  });
 
   return {
     days,
