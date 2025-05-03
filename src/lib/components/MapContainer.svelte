03bf2e8d7aa568a48f7b3841782c7e42811f290b
Dwight Van Tuyl <dvantuyl@gmail.com>
2025-05-03 14:55:19 -0700

    increase max zoom

--- src/lib/components/MapContainer.svelte
+++ src/lib/components/MapContainer.svelte
@@ -33,6 +33,7 @@
 
 			// Add OpenStreetMap tile layer
 			L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+				maxZoom: 20,
 				attribution:
 					'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 			}).addTo(mapInstance);
