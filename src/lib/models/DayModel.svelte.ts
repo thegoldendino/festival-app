f12c1b207e143efe2a26b198bb3f574846d34a88
Dwight Van Tuyl <dvantuyl@gmail.com>
2025-05-10 14:19:02 -0700

    New DayModel

--- src/lib/models/DayModel.svelte.ts
+++ src/lib/models/DayModel.svelte.ts
@@ -18,14 +18,30 @@ export default class DayModel {
 		return new DayModel(day);
 	}
 
-	scheduleGrid(): string[][] {
+	timeFor(idx: number): Date {
+		const start = new Date(this.startTime);
+		start.setMinutes(start.getMinutes() + idx * Number(this.scheduleIncrement));
+		return start
+	}
+
+	activeFor(idx: number): boolean {
+		const start = new Date(this.startTime);
+		start.setMinutes(start.getMinutes() + idx * Number(this.scheduleIncrement));
+		const end = new Date(this.startTime);
+		end.setMinutes(end.getMinutes() + (idx + 1) * Number(this.scheduleIncrement));
+		const now = new Date();
+		return now >= start && now < end;
+	}
+
+	get scheduleGrid(): string[][] {
 		const schedule: string[][] = [];
 		const startTime = new Date(this.startTime);
 		const endTime = new Date(this.endTime);
 		const increment = Number(this.scheduleIncrement);
 
-		for (let time = startTime; time <= endTime; time.setMinutes(time.getMinutes() + increment)) {
-			schedule.push([time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })]);
+		for (let time = startTime; time < endTime; time.setMinutes(time.getMinutes() + increment)) {
+			const timeSlot = time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+			schedule.push([timeSlot,]);
 		}
 
 		return schedule;
